751fdb912da664d419af7e39d56fa0f0ab3f2600
shaoguangleo <shaoguangleo@gmail.com>
2016-03-07 18:41:43 +0800

    Using of escape characters

---
+++ escape_char.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout << "\aWelcome to \"LetsProgramming\" Forum!\n";
+    cout << "Enter your age :<_______>\b\b\b\b\b\b\b";
+    short name;
+    cin >> name;
+    cout << "\aYou are " << name  << " years old"<< endl;
+    return 0;
+}
