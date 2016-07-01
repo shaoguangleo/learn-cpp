595e0193d325d108ec1f5d7069c85fa9eb78c7ab
shaoguangleo <shaoguangleo@gmail.com>
2016-07-01 12:27:33 +0800

    Adding cout dec hex and oct numbers

---
+++ dec-hex-oct/dec_hex_oct.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    const int a = 150;
+    cout << " a number in decimal:" << dec << a << endl;
+
+    cout << "hex: " << hex << a << endl;
+    cout << "oct: " << oct << a << endl;
+
+    return 0;
+}
